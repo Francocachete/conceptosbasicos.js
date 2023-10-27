7294965a03dbade7587e5a84ff519290d51af29d
Franco Iñigo <franco.millo77@gmail.com>
2023-10-26 22:18:59 -0300

    comenzamos con lo basico de js

---
+++ js/01-variables.js
@@ -0,0 +1,14 @@
+// esto es un comentario de una unica linea
+
+/* esto es un comentario
+de multiples 
+lineas */
+
+// declarar una variable (var - let - const)
+
+let anioActual = 2023;
+
+//mostrar mensajes
+console.log(anioActual);
+console.log("anioActual");
+console.log("año Actual: " + anioActual);
