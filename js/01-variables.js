a6bd5079d5a44c82207f3a3077dae704164c2093
Franco Iñigo <franco.millo77@gmail.com>
2023-10-29 11:41:44 -0300

    agregamos un mensaje con document.write y un alert

--- js/01-variables.js
+++ js/01-variables.js
@@ -7,8 +7,20 @@ lineas */
 // declarar una variable (var - let - const)
 
 let anioActual = 2023;
+const universe = "https://universe.rollingcodeschool.com/";
 
 //mostrar mensajes
 console.log(anioActual);
 console.log("anioActual");
 console.log("año Actual: " + anioActual);
+console.log(universe)
+
+//modificar el contenido de una variable
+anioActual = 2024;
+console.log(anioActual);
+
+//como mostrar en la web, en el html un mensaje
+document.write("<p class='parrafoDestacado'>esto es una prueba de document.write</p>")
+
+//ventanas emergentes
+alert("Hola mundo");
