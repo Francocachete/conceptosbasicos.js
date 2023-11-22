944f8eedb213b2bf6c29ca63a40f6ce1e1d3f549
Franco Iñigo <franco.millo77@gmail.com>
2023-11-22 10:29:33 -0300

    terminamos funciones

--- js/06-funciones.js
+++ js/06-funciones.js
@@ -1,18 +1,34 @@
-//funciones declarativas
+//funciones declarativas (puede o no tener parametros) funciones deben ser especificas hacer una sola cosa
 function saludar(){
   //todo el codigo que sabe hacer esa funcion
 document.write("<p>hola mundo</p>");
 }
 
+//funcion con parametros, cuando hay que enviarle un dato a la funcion, no hay limites de parametros
 function saludoNuevo(nombreSolicitado, apellidoSolicitado){
     document.write(`<p>hola ${nombreSolicitado}, ${apellidoSolicitado}</p>`);
 }
 
-function multiplicar(numero1, numero2){
-    const resultado = numero1 * numero2;
-    console.log(resultado);
-    return resultado;
-}
+
+//function multiplicar(numero1, numero2){
+//    const resultado = numero1 * numero2;
+//    console.log(resultado);
+//    return resultado;
+//}
+
+
+
+//arrow functions(esta es otra funcion, hay forma larga como la primera y la segunda es la corta) 
+//const multiplicar = (numero1, numero2) =>{
+//  const resultado = numero1 * numero2;
+//  console.log(resultado);
+//      return resultado;
+//}
+
+
+const multiplicar = (numero1, numero2) => numero1 * numero2; //el return esta implicitamente
+
+
 
 //llamar o invocar a una funcion
 saludar();
@@ -21,7 +37,7 @@ const nombre = prompt("ingrese un nombre");
 const apellido = prompt("ingrese un apellido");
 
 saludoNuevo(nombre, apellido);
-
+saludoNuevo("harry", "potter");
 
 
 const resultadoNuevo = multiplicar(30,3);
